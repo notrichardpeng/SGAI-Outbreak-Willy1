3cac35d7053159bd5fb79297eaa1de8f77aa67ba
notrichardpeng <notrichardpeng@gmail.com>
2022-07-29 18:04:43 -0400

    Make DataCollector static

--- SGAI_MK3/DataCollector.py
+++ SGAI_MK3/DataCollector.py
@@ -1,16 +1,23 @@
-class DataCollector:
-    def __init__(self, hospital=False):
-        self.hospital = hospital
-        self.reset()
+class DataCollector:    
 
-    def reset(self):
-        self.zombies_killed = 0
-        self.zombies_cured = 0
-        self.zombies_cured_in_hospital = 0
-        self.human_vaccinated = 0
-        self.turns_taken = 0
-        self.humans_remaining = 0
+    hospital = False
+    zombies_killed = 0
+    zombies_cured = 0
+    zombies_cured_in_hospital = 0
+    human_vaccinated = 0
+    turns_taken = 0
+    humans_remaining = 0
 
+    @staticmethod
+    def reset():
+        DataCollector.zombies_killed = 0
+        DataCollector.zombies_cured = 0
+        DataCollector.zombies_cured_in_hospital = 0
+        DataCollector.human_vaccinated = 0
+        DataCollector.turns_taken = 0
+        DataCollector.humans_remaining = 0
+
+    @staticmethod
     def save_player_data(self):
         with open("previous_game_data.txt", "w") as f:
             lines = [
@@ -19,10 +26,11 @@ class DataCollector:
                 "Zombies cured: " + str(self.zombies_cured) + "\n",
                 "Zombies cured in hospital: " + str(self.zombies_cured_in_hospital) + "\n",
                 "Humans vaccinated: " + str(self.human_vaccinated) + "\n",
-                "Humans remaining: " + str(self.humans_remaining) + + "\n",
-                "Number of turns taken: " + str(self.turns_taken) + + "\n"
+                "Humans remaining: " + str(self.humans_remaining) + "\n",
+                "Number of turns taken: " + str(self.turns_taken) + "\n"
             ]
             f.writelines(lines)
 
+    @staticmethod
     def save_ai_data(self):
         pass
