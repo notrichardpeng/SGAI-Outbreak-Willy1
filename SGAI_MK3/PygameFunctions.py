57ce0370e09bb6742e8935a4da5b02774fb9da30
Sol Song <solee102@gmail.com>
2022-07-29 14:36:12 -0400

    Additions to Start Screen

--- SGAI_MK3/PygameFunctions.py
+++ SGAI_MK3/PygameFunctions.py
@@ -154,6 +154,10 @@ def display_start_screen(starthover):
     screen.blit(
          text_image, (375, 100),
     )
+    image = pygame.image.load("Assets/person_normal_big.png")
+    screen.blit(image, (900,350))
+    image = pygame.image.load("Assets/person_zombie_big.png")
+    screen.blit(image, (700,350))
     if starthover == "start":
         display_image(screen, "Assets/start_img_2.png", (300, 100), (455, 500))
     else:
